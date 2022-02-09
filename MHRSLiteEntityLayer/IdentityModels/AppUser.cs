608525f0e7825f0ec8fb88acf0d9f9b720634f3a
merveakin <merveakinnnn@hotmail.com>
2022-02-09 10:58:27 +0300

    Add project files.

---
+++ MHRSLiteEntityLayer/IdentityModels/AppUser.cs
@@ -0,0 +1,18 @@
+﻿using MHRSLiteEntityLayer.Enums;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MHRSLiteEntityLayer.IdentityModels
+{
+    public class AppUser : IdentityUser
+    {
+        public string Picture { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public Genders Gender { get; set; }
+
+    }
+}

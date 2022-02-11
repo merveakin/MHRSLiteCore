82d01b7908ed14587de980610e4412cfebc64310
merveakin <merveakinnnn@hotmail.com>
2022-02-11 13:00:25 +0300

    Repositoryler oluşturulmaya başlandı.

---
+++ MHRSLiteBusinessLayer/Implementations/CityRepository.cs
@@ -0,0 +1,21 @@
+﻿using MHRSLiteBusinessLayer.Contracts;
+using MHRSLiteDataAccessLayer;
+using MHRSLiteEntityLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MHRSLiteBusinessLayer.Implementations
+{
+    public class CityRepository : Repository<City>, ICityRepository
+    {
+        private readonly MyContext _myContext;
+
+        public CityRepository(MyContext myContext) : base(myContext)
+        {
+
+        }
+    }
+}
---
+++ MHRSLiteBusinessLayer/Implementations/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using MHRSLiteBusinessLayer.Contracts;
+using MHRSLiteDataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MHRSLiteBusinessLayer.Implementations
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly MyContext _myContext;
+        public UnitOfWork(MyContext myContext)
+        {
+            _myContext = myContext;
+            //UnitOfWork tüm repositoryleri oluşturacak.
+            CityRepository = new CityRepository(_myContext);
+        }
+
+        public ICityRepository CityRepository { get; private set; }
+
+        public void Dispose()
+        {
+            _myContext.Dispose();
+        }
+    }
+}

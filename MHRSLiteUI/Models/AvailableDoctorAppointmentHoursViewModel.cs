9b5682f813052a884098fe0903a8266f340057c0
merveakin <merveakinnnn@hotmail.com>
2022-02-23 14:59:12 +0300

    Randevu arayınca doktora göre saatlerin listeleneceği sayfaya başlandı.

---
+++ MHRSLiteUI/Models/AvailableDoctorAppointmentHoursViewModel.cs
@@ -0,0 +1,21 @@
+﻿using MHRSLiteEntityLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MHRSLiteUI.Models
+{
+    public class AvailableDoctorAppointmentHoursViewModel
+    {
+        //DR ADI
+        //TARİH --> YARIN SEÇİLSİN
+        //SAAT BAŞLARI
+        //SAAT DETAYLARI
+        public Doctor Doctor { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public string HourBase { get; set; }
+        public List<string> Hours { get; set; } = new List<string>();
+
+    }
+}

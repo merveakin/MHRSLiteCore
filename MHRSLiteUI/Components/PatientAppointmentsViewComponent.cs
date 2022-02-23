fec2784df75252aa5081ab671b28c5c5871351be
merveakin <merveakinnnn@hotmail.com>
2022-02-23 11:44:45 +0300

    Geçmiş ve gelecek randevular Patient/Index sayfasına View Component aracılığı ile getirildi.

---
+++ MHRSLiteUI/Components/PatientAppointmentsViewComponent.cs
@@ -0,0 +1,102 @@
+﻿using MHRSLiteBusinessLayer.Contracts;
+using MHRSLiteUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MHRSLiteUI.Components
+{
+    public class PatientAppointmentsViewComponent : ViewComponent
+    {
+        //GLOBAL ALAN
+        private readonly IUnitOfWork _unitOfWork;
+
+        //Dependency Injection
+        public PatientAppointmentsViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            PastAndFutureAppointmentsViewModel data =
+                new PastAndFutureAppointmentsViewModel();
+
+            //23.02.2022
+            DateTime today = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+
+            //data.UpcomingAppointments = _unitOfWork
+            //   .AppointmentRepository
+            //   .GetAll(x =>
+            //   x.PatientId == HttpContext.User.Identity.Name
+            //   &&
+            //   x.AppointmentDate >= today
+            //   &&
+            //  (Convert.ToInt32(x.AppointmentHour.Substring(0, 2)) >
+            //   DateTime.Now.Hour
+            //   ||
+            //   (
+            //    Convert.ToInt32(x.AppointmentHour.Substring(0, 2)) ==
+            //   DateTime.Now.Hour
+            //   &&
+            //   Convert.ToInt32(x.AppointmentHour.Substring(3, 2)) >=
+            //   DateTime.Now.Minute
+            //   ))
+            //   , includeProperties: "HospitalClinic"
+            //   ).ToList();
+
+            //data.PastAppointments = _unitOfWork.AppointmentRepository.GetAll(x =>
+            //x.PatientId == HttpContext.User.Identity.Name
+            //&&
+            //x.AppointmentDate <= today
+            // &&
+            // (Convert.ToInt32(x.AppointmentHour.Substring(0, 2)) <
+            //   DateTime.Now.Hour
+            //   ||
+            //   (
+            //    Convert.ToInt32(x.AppointmentHour.Substring(0, 2)) ==
+            //   DateTime.Now.Hour
+            //   &&
+            //   Convert.ToInt32(x.AppointmentHour.Substring(3, 2)) <
+            //   DateTime.Now.Minute
+            //   ))
+            //, includeProperties: "HospitalClinic").ToList();
+
+
+            data.UpcomingAppointments = _unitOfWork.AppointmentRepository
+                .GetAll(x =>
+                x.PatientId == HttpContext.User.Identity.Name 
+                && 
+                x.AppointmentDate > today 
+                ||
+                (x.AppointmentDate == today 
+                &&
+                (Convert.ToInt32(x.AppointmentHour.Substring(0, 2)) > DateTime.Now.Hour 
+                ||
+                (Convert.ToInt32(x.AppointmentHour.Substring(0, 2)) == DateTime.Now.Hour
+                && 
+                Convert.ToInt32(x.AppointmentHour.Substring(3, 2)) >= DateTime.Now.Minute)))
+                , includeProperties: "HospitalClinic").ToList();
+
+            data.PastAppointments = _unitOfWork.AppointmentRepository
+                .GetAll(x =>
+                x.PatientId == HttpContext.User.Identity.Name
+                &&
+                x.AppointmentDate <= today
+                && 
+                (x.AppointmentDate == today
+                &&
+                (Convert.ToInt32(x.AppointmentHour.Substring(0, 2)) < DateTime.Now.Hour 
+                || 
+                (Convert.ToInt32(x.AppointmentHour.Substring(0, 2)) == DateTime.Now.Hour 
+                &&
+                Convert.ToInt32(x.AppointmentHour.Substring(3, 2)) < DateTime.Now.Minute)))
+                , includeProperties: "HospitalClinic").ToList();
+
+
+            return View(data);
+        }
+    }
+}

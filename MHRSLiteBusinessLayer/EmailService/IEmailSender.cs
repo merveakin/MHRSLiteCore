af135f592d81dcfca673457a883411df6f907dff
merveakin <merveakinnnn@hotmail.com>
2022-02-28 13:09:35 +0300

    Hastanın randevusunu pdf halinde göndermek için metotlar eklendi.

--- MHRSLiteBusinessLayer/EmailService/IEmailSender.cs
+++ MHRSLiteBusinessLayer/EmailService/IEmailSender.cs
@@ -1,4 +1,5 @@
 ﻿using MHRSLiteEntityLayer;
+using MHRSLiteEntityLayer.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,10 @@ namespace MHRSLiteBusinessLayer.EmailService
     public interface IEmailSender
     {
         Task SendAsync(EmailMessage message);
+
+        void SendAppointmentPdf(EmailMessage message,AppointmentVM data);
+
+        Task SendAppointmentPdfAsync(EmailMessage message, AppointmentVM data);
 
     }
 }

c3be9c4acc119b81e93fbb00941166dccab52f0d
merveakin <merveakinnnn@hotmail.com>
2022-02-28 10:01:06 +0300

    AutoMapper eklendi. Sayfalama yapabilmek için PaginatedList eklendi.

---
+++ MHRSLiteEntityLayer/Mappings/Maps.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using MHRSLiteEntityLayer.Models;
+using MHRSLiteEntityLayer.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MHRSLiteEntityLayer.Mappings
+{
+    public class Maps : Profile
+    {
+        public Maps()
+        {
+            //Appointment'ı AppointmentVM'ye dönüştür
+            //CreateMap<Appointment, AppointmentVM>();
+            //AppointmentVM'yi Appointment'a dönüştür
+            //CreateMap<AppointmentVM, Appointment>();
+
+            //ReverseMap sayesinde yukarıdaki 2 ayrı işlemi tek satırda yapmış oluyoruz.
+            //Appointment AppointmentVM'ye dönüşebilir.
+            //AppointmentVM Appointment'a dönüşebilir.
+            CreateMap<AppointmentVM, Appointment>().ReverseMap();
+
+        }
+    }
+}
---
+++ MHRSLiteEntityLayer/PagingListModels/PaginatedList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MHRSLiteEntityLayer.PagingListModels
+{
+    public class PaginatedList<T> : List<T>
+    {
+        public int PageIndex { get; set; }
+        public int TotalPages { get; set; }
+        public List<T> ItemList { get; set; }
+
+        public PaginatedList(List<T> items, int count, int pageindex, int pageSize)
+        {
+            PageIndex = pageindex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            ItemList = items;
+        }
+        public bool PreviousPage
+        {
+            get
+            {
+                return (PageIndex > 1);
+            }
+        }
+
+        public bool NextPage
+        {
+            get
+            {
+                return (PageIndex < TotalPages);
+            }
+        }
+        public static PaginatedList<T> CreateAsync(List<T> sourcelist, int pageindex, int pageSize)
+        {
+            var count = sourcelist.Count();
+            //Bulunduğu sayfadan bir azaltıp sayfada kaç eleman olacaksa o kadar veriyi atlar.
+            var items = sourcelist.Skip((pageindex - 1) * pageSize)
+                //sayfada kaç eleman olmasını istiyorsak o kadarını alır.
+                .Take(pageSize)
+                //listele
+                .ToList();
+            //oluşan listeyi yeni nesne yaratarak gönderir.
+            return new PaginatedList<T>(items, count, pageindex, pageSize);
+
+
+        }
+
+    }
+}
